318d1868af3328761c9ce00103f7acf7f97da32f
Ernest Bennett <93333835+melonbulb@users.noreply.github.com>
2025-11-07 14:17:58 +0800

    move logic to createTile function

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 const mapSize = 10;
 
-function generateMap() {
-    const map = [];
-    // TODO: Generate location of boms and adjacent bom counts
-    document.getElementById('map').style.setProperty('--size', mapSize);
-    for (let x = 0; x < mapSize; x++) {
-        const row = [];
-        for (let y = 0; y < mapSize; y++) {
-            const tileElement = document.createElement('div');
-            tileElement.classList.add('tile');
-            tileElement.dataset.status = 'hidden';
-            tileElement.addEventListener('click', () => {
-                tileElement.dataset.status = 'empty'; // TODO: update based on actual position value (bomb or number of adjacent bombs)
-            });
-            document.getElementById('map').appendChild(tileElement);
-        }
-        map.push(row);
-    }
-
-    return map;
+function createTile(status){
+    const tileElement = document.createElement('div');
+    tileElement.classList.add('tile');
+    tileElement.dataset.status = 'hidden';
+    tileElement.addEventListener('click', () => {
+        tileElement.dataset.status = status;
+    });
+    document.getElementById('map').appendChild(tileElement);
 }
 
-generateMap();
+function createMap() {
+    document.getElementById('map').style.setProperty('--size', mapSize);
+    let map = [];
+    // TODO: Generate location of bombs and adjacent bomb counts
+    for (let x = 0; x < mapSize; x++) {
+        // let row = [];
+        for (let y = 0; y < mapSize; y++) {
+            createTile("empty");
+            // row.push(tileMeta);
+        }
+        // map.push(row);
+    }
+    // return map;
+}
+
+let data = createMap();
